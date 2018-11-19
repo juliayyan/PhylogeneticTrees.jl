bf38610bb909de707b1e87e78d99a27db5c20507
juliayyan <juliayunqian@gmail.com>
2018-11-19 15:17:58 -0500

    Add test to compare models

--- test/comparetrees.jl
+++ test/comparetrees.jl
@@ -11,9 +11,34 @@ module CompareTrees
     println("Comparing both models")
     println("=====================")
 
-    @testset "" begin
+    @testset "Comparing admixture models" begin
 
-    	
+        pd = PhylogeneticTrees.PopulationData(
+            "testdata/f3.Europe6.csv",
+            "testdata/f3.Europe6-covariance.csv"
+            )
+
+        nlevels = 2
+        depth = 4
+        bt = PhylogeneticTrees.BinaryTree(depth)
+
+        tp = PhylogeneticTrees.TreeProblem(pd, bt,
+            solver = GurobiSolver(OutputFlag = 0, TimeLimit = 1*60),
+            nlevels = nlevels)
+        PhylogeneticTrees.warmstartunmixed(tp)
+        @time solve(tp.model)
+        # 60.237210 seconds (53.08 k allocations: 9.018 MiB)
+
+        tp2 = PhylogeneticTrees.NodeTreeProblem(pd, bt,
+            nlevels = nlevels, 
+            solver = GurobiSolver(OutputFlag = 0))
+        for p in 1:pd.npop, u in PhylogeneticTrees.getleaves(bt), l in 1:nlevels 
+            JuMP.@constraint(tp2.model, tp2.assign[p,u,l] == getvalue(tp.assign[p,u,l]))
+        end
+        @time solve(tp2.model)
+        # 0.617204 seconds (155 allocations: 47.423 MiB, 24.36% gc time)
+
+        @test isapprox(getobjectivevalue(tp2.model), getobjectivevalue(tp.model))
 
     end
 
